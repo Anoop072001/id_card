0c9f23f0bdc1c7681b8a701aad0f8a56abfddf86
Anoop <anoopsidhan777@gmail.com>
2021-04-29 22:51:19 +0530

    id

---
+++ android/app/src/main/kotlin/anoop/com/id_card/MainActivity.kt
@@ -0,0 +1,6 @@
+package anoop.com.id_card
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Idcard(),
+),);
+
+class Idcard extends StatefulWidget {
+  @override
+  _IdcardState createState() => _IdcardState();
+}
+
+class _IdcardState extends State<Idcard> {
+
+  int current_Year=1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text(
+          "ID Card"
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            current_Year+=1;
+          });
+        },
+        child: Icon(
+          Icons.add,
+        ),
+        backgroundColor: Colors.grey[800],
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/Anoop.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 90.0,
+              color: Colors.grey[800],
+            ),
+            Text("NAME",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 1.5,
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Text("Anoop",
+              style: TextStyle(
+                color: Colors.red[400],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Text("CURRENT YEAR",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 1.5,
+              ),
+            ),
+            SizedBox(height: 10.0,),
+            Text("$current_Year",
+              style: TextStyle(
+                color: Colors.red[400],
+                letterSpacing: 2.0,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey,
+                ),
+                SizedBox(width: 10.0,),
+                Text(
+                  "anoopsidhan777@gmail.com",
+                    style: TextStyle(
+                      color: Colors.red[400],
+                      fontSize: 18.0,
+                      letterSpacing: 1.0,
+                    ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
