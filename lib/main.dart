c381f89d7f525387a58e1b09307f186af4683e89
Anoop <anoopsidhan777@gmail.com>
2021-06-13 13:36:47 +0530

    animated text added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Idcard(),
-),);
+void main() => runApp(
+      MaterialApp(
+        home: Idcard(),
+      ),
+    );
 
 class Idcard extends StatefulWidget {
   @override
@@ -10,16 +13,13 @@ class Idcard extends StatefulWidget {
 }
 
 class _IdcardState extends State<Idcard> {
-
-  int current_Year=1;
+  int current_Year = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text(
-          "ID CARD"
-        ),
+        title: Text("ID CARD"),
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
@@ -27,7 +27,7 @@ class _IdcardState extends State<Idcard> {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           setState(() {
-            current_Year+=1;
+            current_Year += 1;
           });
         },
         child: Icon(
@@ -50,14 +50,18 @@ class _IdcardState extends State<Idcard> {
               height: 90.0,
               color: Colors.grey[800],
             ),
-            Text("NAME",
+            Text(
+              "NAME",
               style: TextStyle(
                 color: Colors.grey,
                 letterSpacing: 1.5,
               ),
             ),
-            SizedBox(height: 10.0,),
-            Text("Anoop",
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "Anoop",
               style: TextStyle(
                 color: Colors.red[400],
                 letterSpacing: 2.0,
@@ -65,15 +69,21 @@ class _IdcardState extends State<Idcard> {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 30.0,),
-            Text("CURRENT YEAR",
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              "CURRENT YEAR",
               style: TextStyle(
                 color: Colors.grey,
                 letterSpacing: 1.5,
               ),
             ),
-            SizedBox(height: 10.0,),
-            Text("$current_Year",
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "$current_Year",
               style: TextStyle(
                 color: Colors.red[400],
                 letterSpacing: 2.0,
@@ -81,22 +91,73 @@ class _IdcardState extends State<Idcard> {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 30.0,),
+            SizedBox(
+              height: 30.0,
+            ),
             Row(
               children: [
                 Icon(
                   Icons.email,
                   color: Colors.grey,
                 ),
-                SizedBox(width: 10.0,),
+                SizedBox(
+                  width: 10.0,
+                ),
                 Text(
                   "anoopsidhan777@gmail.com",
-                    style: TextStyle(
-                      color: Colors.red[400],
-                      fontSize: 18.0,
-                      letterSpacing: 1.0,
-                    ),
+                  style: TextStyle(
+                    color: Colors.red[400],
+                    fontSize: 18.0,
+                    letterSpacing: 1.0,
+                  ),
                 ),
+              ],
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Text(
+                  "I'm a",
+                  style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey),
+                ),
+                SizedBox(
+                  height: 100,
+                  width: 20,
+                ),
+                AnimatedTextKit(
+                  animatedTexts: [
+                    TypewriterAnimatedText(
+                      "BTech UnderGrad",
+                      textStyle: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red[400],
+                      ),
+                    ),
+                    TypewriterAnimatedText(
+                      "Flutter developer",
+                      textStyle: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red[400],
+                      ),
+                    ),
+                    TypewriterAnimatedText(
+                      "Figma Designer",
+                      textStyle: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red[400],
+                      ),
+                    ),
+                  ],
+                  isRepeatingAnimation: true,
+                )
               ],
             ),
           ],
